3127743f4d1dd0d8f73aecaea8f84140d496dd97
Natalia Martín Zaballos <nataliamartin@MacBook-Air-de-Natalia.local>
2023-10-25 12:30:31 +0200

    fix: path of main in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import Header from "./Header";
-import Main from "./Main";
+import Main from "./components/Main";
 // import Footer from "./Footer";
 import './App.css';
 
