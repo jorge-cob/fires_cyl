3e6709ec22a97732fe84ec5153b5aae90e732932
Natalia Martín Zaballos <nataliamartin@MacBook-Air-de-Natalia.local>
2023-10-27 12:25:16 +0200

    feat: filtro modelo añadido, falta usarlo con sus props

---
+++ src/components/Filters.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Filter from "./Filter";
+
+function Filters() {
+//TODAVÍA NO SÉ CÓMO SE RELACIONA CON LA TABLA Y TENGO Q PENSAR LAS PROPS
+    return (
+        <>
+            <Filter/>
+            <Filter/>
+            <Filter/>
+            <Filter/>
+        </>
+    )
+}
+export default Filters;
